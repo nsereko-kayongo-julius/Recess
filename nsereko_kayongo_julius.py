c58f279efe57f77e4952b3a7c24dbddb5f0c0a03
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-06-20 11:58:14 +0300

    introduction to python

---
+++ nsereko_kayongo_julius.py
@@ -0,0 +1,55 @@
+"""
+python script
+"""
+name = 'Nserweko'
+age = 21
+
+print(name)
+print(age)
+
+#data structures
+"""
+Numeric values from 1 to 10
+integer values and int 
+float values are float
+
+String valures are str
+'Nserweko' 
+
+Boolean values are logical values
+Boolean values are bool
+
+Squence data types
+List - enclosed in square brackets []
+tuple - enclosed in round brackets ()
+range - often used in for loops, iteration
+
+Mapping data types
+Dictionary - enclosed in curly brackets {}
+Set - enclosed in round brackets ()
+
+None - null value
+
+"""
+
+#list
+fruits = ['apple', 'banana', 'orange', 'mango']
+print(fruits)
+
+#tuple
+fruits = ('apple', 'banana', 'orange', 'mango')
+print(fruits)
+
+#range
+numbers = range(1, 10)
+print(numbers)
+
+#dictionary
+person = {'name': 'Nserweko', 'age': 21}
+
+#set
+fruits = {'apple', 'banana', 'orange', 'mango'}
+
+#None
+person = None
+
